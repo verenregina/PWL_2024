08fb632d3ee8819497bc7dd36371337a85ca3383
verenregina <reginaveren62@gmail.com>
2025-02-17 15:38:08 +0700

    view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-	public function hello() {
-    	    return 'Hello World';
-	}
+    public function hello(){
+        return 'Hello World';
+    }
+
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name', 'Veren Regina')
+            ->with('occupation', 'Astronaut');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -93,3 +93,13 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+// // // // // // // // // // // // // // // // // // // //
+Route::get('/greeting', function () {
+	return view('blog.hello', ['name' => 'Veren']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
